2787aec28ce71140448702d852f8f3d9e3b89a11
JimCat <jim19901103@gmail.com>
2023-01-13 00:15:22 +0800

    update to try

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 echo "INPUT_MONGODB_URI=$db_uri" >> $GITHUB_OUTPUT
+echo "INPUT_MONGODB_NAME=$db_name" >> $GITHUB_OUTPUT
 
 BACKUP_DIR="backups"
   if [ ! -d ./$BACKUP_DIR/ ]; then
@@ -8,8 +9,13 @@ BACKUP_DIR="backups"
   fi
 
 echo $INPUT_MONGODB_URI
+echo $INPUT_MONGODB_NAME
 
-mongodump --uri $INPUT_MONGODB_URI -o=./$BACKUP_DIR
+mongodump --uri $INPUT_MONGODB_URI$INPUT_MONGODB_NAME -o=./$BACKUP_DIR
 
 echo "Show me backups:"
+ls -lFhS ./$BACKUP_DIR/
+
+tar zcvf $INPUT_MONGODB_NAME.tar.gz $BACKUP_DIR
+
 ls -lFhS ./$BACKUP_DIR/
