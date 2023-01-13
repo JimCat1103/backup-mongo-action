cd77f796374941debc84d5816986edf3b472e0b4
JimCat <jim19901103@gmail.com>
2023-01-14 01:17:33 +0800

    try split

--- backup.sh
+++ backup.sh
@@ -12,8 +12,7 @@ echo "exec_type=$exec_type" >> $GITHUB_OUTPUT
 echo "INPUT_MONGODB_URI=$mongodb_uri" >> $GITHUB_OUTPUT
 
 TYPE=$exec_type
-
-if [ "$TYPE" = "multi" ]; then
+if [ "$TYPE" == "multi" ]; then
   IFS=","
   array=($string)
   for element in "${array[@]}"
