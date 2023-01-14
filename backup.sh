2ff2bfa656e1dd90689ef9696aa1911fe7c8caf1
JimCat <jim19901103@gmail.com>
2023-01-14 10:34:09 +0800

    trace

--- backup.sh
+++ backup.sh
@@ -11,12 +11,13 @@ fi
 echo "EXEC_TYPE=$exec_type" >> $GITHUB_OUTPUT
 echo "INPUT_MONGODB_URI=$mongodb_uri" >> $GITHUB_OUTPUT
 
-IFS=',' read -r -a array <<< $INPUT_MONGODB_URI
-for i in "${array[@]}"
-do
-  echo "this in $i"
-#     mongodump --uri $i -o=./$BACKUP_DIR
-done
+if [ "$EXEC_TYPE" | grep -w "multi" ]; then
+  IFS=',' read -r -a array <<< $INPUT_MONGODB_URI
+  for i in "${array[@]}" ; do
+    echo "this in $i"
+  done
+fi
+
 
 echo "Show me backups:"
 ls -lFhS ./$BACKUP_DIR/
