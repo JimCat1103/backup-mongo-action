b3b219ecbc79e2c9b32a8e92c9fe5b4cf01edf32
JimCat <jim19901103@gmail.com>
2023-01-14 11:29:49 +0800

    trace

--- backup.sh
+++ backup.sh
@@ -8,12 +8,12 @@ else
   mkdir $BACKUP_DIR
 fi
 
-echo "TAG=$exec_type" >> $GITHUB_OUTPUT
 echo "INPUT_MONGODB_URI=$mongodb_uri" >> $GITHUB_OUTPUT
+echo "TAG=$exec_type" >> $GITHUB_OUTPUT
 
 echo "type is $TAG, $exec_type"
 
-if [ "$TAG" = "multi" ]; then
+if [ "$TAG" = "" ]; then
   IFS=',' read -r -a array <<< $INPUT_MONGODB_URI
   for i in "${array[@]}" ; do
     echo "this in $i"
